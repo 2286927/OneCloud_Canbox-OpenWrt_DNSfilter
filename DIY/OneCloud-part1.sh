d339760b2f01400454052eeae82c14953e48e6ce
ChenChen <cc2286927@sina.cn>
2022-03-08 08:27:07 +0800

    Update OneCloud-part1.sh

--- DIY/OneCloud-part1.sh
+++ DIY/OneCloud-part1.sh
@@ -24,8 +24,9 @@ sed -i 's/OpenWrt/OneCloud/g' package/base-files/files/bin/config_generate
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default

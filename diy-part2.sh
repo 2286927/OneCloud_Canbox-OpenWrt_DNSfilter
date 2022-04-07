af6f396493cc39ed4b5f523841d5f3ac1e472109
ChenChen <cc2286927@sina.cn>
2022-04-07 08:20:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@ sed -i 's/OpenWrt/CM520-79F/g' package/base-files/files/bin/config_generate
 # 修改机器初始密码
 sed -i 's/root:.*:0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
 # zerotier
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' openwrt/feeds/packages/net/zerotier/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile

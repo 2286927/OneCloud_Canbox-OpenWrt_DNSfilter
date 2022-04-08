cc826e0bd270cba2c5d8b936253a52426b2558f5
ChenChen <cc2286927@sina.cn>
2022-04-08 11:40:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,8 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# Modify default IP
+sed -i 's/192.168.1.1/8.1.1.1/g' package/base-files/files/bin/config_generate
 # 修改机器名称
-sed -i 's/OpenWrt/CM520-79F/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/CM520-79F-Two/g' package/base-files/files/bin/config_generate
 # 修改机器初始密码
 sed -i 's/root:.*:0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
 # zerotier

7da031c5adf32bcc099c23698a8bf549c6290144
ChenChen <cc2286927@sina.cn>
2022-04-13 12:48:55 +0800

    Update diy-part2.sh

--- DIY/diy-part2.sh
+++ DIY/diy-part2.sh
@@ -6,3 +6,13 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
+sed -i 's/192.168.1.1/8.1.1.1/g' package/base-files/files/bin/config_generate
+# 修改机器名称
+sed -i 's/OpenWrt/CM520-79F-Two/g' package/base-files/files/bin/config_generate
+# 修改机器初始密码
+# sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
+# zerotier
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
+#sed -i 's/PKG_HASH:=.*/PKG_HASH:=2866a4ef9193cca0a9f0fe528a0dea00c13cb0fd714bf388a0300cb6f3639b3b/g' feeds/packages/net/zerotier/Makefile
+#rm -rf feeds/packages/net/zerotier
+#cp -rf $GITHUB_WORKSPACE/general/zerotier feeds/packages/net

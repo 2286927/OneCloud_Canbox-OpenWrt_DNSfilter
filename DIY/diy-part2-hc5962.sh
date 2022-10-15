7bbcee2acdbff8e8105f63a88872b20887d7ac9b
ChenChen <cc2286927@sina.cn>
2022-10-15 17:18:25 +0800

    Update diy-part2-hc5962.sh

--- DIY/diy-part2-hc5962.sh
+++ DIY/diy-part2-hc5962.sh
@@ -7,6 +7,8 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/9.1.1.1/g' package/base-files/files/bin/config_generate
+# 修改机器名称
+sed -i 's/OpenWrt/HiWiFi4E.com/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
 # mkdir package/base-files/files/config

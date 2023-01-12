ec67948d40fd510e60f7d534378a2d05e5e5e61e
ChenChen <cc2286927@sina.cn>
2023-01-12 13:30:53 +0800

    Update diy-part1.sh

--- DIY/diy-part1.sh
+++ DIY/diy-part1.sh
@@ -33,7 +33,8 @@
 #git clone https://github.com/kiddin9/openwrt-packages package/kiddin9
 git clone https://github.com/kenzok8/small-package package/small-package
 #git clone https://github.com/bootli/luci-app-turboacc.git package/small-package/luci-app-turboacc
-git clone https://github.com/deplives/luci-app-turboacc.git package/small-package/luci-app-turboacc
+#git clone https://github.com/deplives/luci-app-turboacc.git package/small-package/luci-app-turboacc
+git clone https://github.com/imapproxy/luci-app-turboacc.git package/small-package/luci-app-turboacc
 git clone https://github.com/bootli/luci-app-ddns-go.git package/ddns-go
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/dockerman
 #  git clone https://github.com/kenzok8/small-package package/small-package

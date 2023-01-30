ca77a493bfa5befdbd48477e42f3f55f7e142861
ChenChen <cc2286927@sina.cn>
2023-01-30 10:40:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,7 +33,10 @@
 git clone https://github.com/kenzok8/small-package package/small-package
 #git clone https://github.com/bootli/luci-app-turboacc.git package/small-package/luci-app-turboacc
 #git clone https://github.com/imapproxy/luci-app-turboacc.git package/small-package/luci-app-turboacc
+## Turbo ACC
 svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/small-package/luci-app-turboacc
+svn export --force https://github.com/kiddin9/openwrt-packages/trunk/shortcut-fe package/small-package/luci-app-turboacc/shortcut-fe
+#
 svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-rclone package/small-package/luci-app-rclone
 git clone https://github.com/bootli/luci-app-ddns-go.git package/ddns-go
 ##  sed -i '$a src-git dnsfilter https://github.com/kiddin9/luci-app-dnsfilter' feeds.conf.default

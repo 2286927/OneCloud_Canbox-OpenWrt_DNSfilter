2b09c35251cf655383c0cc2681b14014097b025b
ChenChen <cc2286927@sina.cn>
2023-03-13 16:49:36 +0800

    Update OneCloud-part2.sh

--- DIY/OneCloud-part2.sh
+++ DIY/OneCloud-part2.sh
@@ -18,6 +18,14 @@ sed -i 's/192.168.1.1/172.16.1.31/g' package/base-files/files/bin/config_generat
 sed -i 's/OpenWrt/OneCloud/g' package/base-files/files/bin/config_generate
 # 修改机器初始密码
 # sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
+####### Modify the version number
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='OpenWrt $('+%V')'" >> package/base-files/files/etc/openwrt_release
+sed -i "s/OpenWrt /洲·Cy build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/base-files/files/etc/openwrt_release
+# DNS Cache FixUP
+echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
+# 修改连接数
+sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=165535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default

6f715eb0e1ce4851b57f5a8aeee15c73338a64bb
ChenChen <cc2286927@sina.cn>
2023-05-14 15:56:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@ sed -i 's/192.168.1.1/172.30.1.1/g' package/base-files/files/bin/config_generate
 # 修改机器名称
 sed -i 's/OpenWrt/CM520-79F-Two/g' package/base-files/files/bin/config_generate
 # 修改机器初始密码
-sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
-sed -i 's/root:x:0:0:root:/root::0:0:root:/g' package/base-files/files/etc/passwd
+#sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
+#sed -i 's/root:x:0:0:root:/root::0:0:root:/g' package/base-files/files/etc/passwd
 # zerotier
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
 # DNS Cache FixUP

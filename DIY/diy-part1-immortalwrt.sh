411c1b292d7ebc0d29c6628d0824323ad53a7583
ChenChen <cc2286927@sina.cn>
2023-06-25 09:38:03 +0800

    Update diy-part1-immortalwrt.sh

--- DIY/diy-part1-immortalwrt.sh
+++ DIY/diy-part1-immortalwrt.sh
@@ -39,9 +39,33 @@ git clone https://github.com/kenzok8/small feeds/small
 #git clone https://github.com/deplives/luci-app-turboacc.git package/small-package/luci-app-turboacc
 #git clone https://github.com/imapproxy/luci-app-turboacc.git package/small-package/luci-app-turboacc
 ## Turbo ACC
-svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/small-package/luci-app-turboacc
-svn export --force https://github.com/kiddin9/openwrt-packages/trunk/shortcut-fe package/small-package/luci-app-turboacc/shortcut-fe
+# svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/small-package/luci-app-turboacc
+# svn export --force https://github.com/kiddin9/openwrt-packages/trunk/shortcut-fe package/small-package/luci-app-turboacc/shortcut-fe
 #
+####################################
+### Turbo ACC-[openwrt(22.03/23.05)]
+####################################
+mkdir -p turboacc_tmp ./package/turboacc
+cd turboacc_tmp 
+git clone https://github.com/chenmozhijin/turboacc -b package
+cd ../package/turboacc
+git clone https://github.com/fullcone-nat-nftables/nft-fullcone
+git clone https://github.com/chenmozhijin/turboacc
+mv ./turboacc/luci-app-turboacc ./luci-app-turboacc
+rm -rf ./turboacc
+cd ../..
+cp turboacc_tmp/turboacc/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-5.15
+cp turboacc_tmp/turboacc/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-5.15
+rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
+mkdir -p ./package/network/config/firewall4 ./package/libs/libnftnl ./package/network/utils/nftables
+cp -r ./turboacc_tmp/turboacc/shortcut-fe ./package/turboacc
+cp -RT ./turboacc_tmp/turboacc/firewall4-04a06bd70b9808b14444cae81a2faba4708ee231/firewall4 ./package/network/config/firewall4
+cp -RT ./turboacc_tmp/turboacc/libnftnl-1.2.5/libnftnl ./package/libs/libnftnl
+cp -RT ./turboacc_tmp/turboacc/nftables-1.0.7/nftables ./package/network/utils/nftables
+rm -rf turboacc_tmp
+####################################
+### Turbo ACC of End
+####################################
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-rclone package/small-package/luci-app-rclone
 # git clone https://github.com/bootli/luci-app-ddns-go.git package/ddns-go
 #git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
